f79fe930e0787c288d609752bd2fb87e53b628c4
Angel Bansal <95308083+abn15@users.noreply.github.com>
2022-10-20 11:44:10 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(MyApps());
+}
+
+class MyApps extends StatelessWidget {
+  Widget bulid(BuildContext context ) {
+    return MaterialApp(home: Text('hello'),);
+  }
+}
